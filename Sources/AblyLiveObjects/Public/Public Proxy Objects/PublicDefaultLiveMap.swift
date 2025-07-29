9c949e5c9b65cf9e386cc5e7c1065b3ebecff7d5
Lawrence Forooghian <lawrence@forooghian.com>
2025-07-29 10:00:12 +0100

    Implement increment / decrement / set / delete
    Based on [1] at cb11ba8.
    
    Channel mode checking and echoMessages check omitted as in 75e201a.
    
    [1] https://github.com/ably/specification/pull/353

--- Sources/AblyLiveObjects/Public/Public Proxy Objects/PublicDefaultLiveMap.swift
+++ Sources/AblyLiveObjects/Public/Public Proxy Objects/PublicDefaultLiveMap.swift
@@ -76,11 +76,11 @@ internal final class PublicDefaultLiveMap: LiveMap {
     }
 
     internal func set(key: String, value: LiveMapValue) async throws(ARTErrorInfo) {
-        try await proxied.set(key: key, value: value)
+        try await proxied.set(key: key, value: value, coreSDK: coreSDK)
     }
 
     internal func remove(key: String) async throws(ARTErrorInfo) {
-        try await proxied.remove(key: key)
+        try await proxied.remove(key: key, coreSDK: coreSDK)
     }
 
     internal func subscribe(listener: @escaping LiveObjectUpdateCallback<LiveMapUpdate>) throws(ARTErrorInfo) -> any SubscribeResponse {
