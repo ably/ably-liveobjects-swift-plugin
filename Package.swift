e61f8eef9bf7bbfc460bce6b9091d345c1bd49c9
Lawrence Forooghian <lawrence@forooghian.com>
2026-02-24 10:57:02 -0300

    WIP bump protocol version to 6
    TODO switch to commits
    TODO describe the process we're following, once written up in
    plugin-support
    TODO explain why broken for now

--- Package.swift
+++ Package.swift
@@ -19,12 +19,12 @@ let package = Package(
     ],
     dependencies: [
         .package(
-            url: "https://github.com/ably/ably-cocoa",
-            from: "1.2.55",
+            name: "ably-cocoa",
+            path: "../ably-cocoa",
         ),
         .package(
-            url: "https://github.com/ably/ably-cocoa-plugin-support",
-            from: "1.0.0",
+            name: "ably-cocoa-plugin-support",
+            path: "../ably-cocoa-plugin-support",
         ),
         .package(
             url: "https://github.com/apple/swift-argument-parser",
